da7345d34ea4c32e30e5b59ee453e1d78566f002
MrFixingIt <daryltwaltz423@gmail.com>
2022-12-15 19:51:02 -0600

    Half Way There!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SQL_in_Action
+
+Column names are:
+id, host_id, host_name, neighbourhood_groud, neighbourhood, latitude, longitude, room_type, price, minimum_rights, number_of_reviews, last_review, reviews_per_month, availability_364, number_of_reviews_ltm
--- sql_in_action-starter.sql
+++ sql_in_action-starter.sql
@@ -1,84 +1,84 @@
 -- <<<<<<<<<<<<<<<<<<<<<<< EXAMPLE >>>>>>>>>>>>>>>>>>>>>>>>
 -- TODO: Remove the "--" from the below SELECT query and run the query
 --    NOTE: When writing queries, make sure each one ends with a semi-colon
-
+-- LIKE THIS
 -- SELECT * FROM final_airbnb;
 
-
-
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>> DONE
 -- Find out how many rows are in the table "final_airbnb"
 -- EXPECTED OUTPUT: 146
 
+-- ---------- SOLUTION ----------
+-- SELECT COUNT(id) as Number_of_rows
+-- FROM final_airbnb
 
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>> DONE
 -- Find out the name of the host for "host_id" 63613
 -- HINT: "Where" could it be?
-
 -- EXPECTED OUTPUT: Patricia
 
+-- ---------- SOLUTION ----------
+-- SELECT host_name FROM final_airbnb
+-- WHERE host_id = 63613
 
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>> DONE
 -- Query the data to just show the unique neighbourhoods listed
 -- HINT: This is a "distinct" operation...
-
 -- EXPECTED OUTPUT: 40 neighbourhoods listed
 
+-- ---------- SOLUTION ----------
+-- SELECT distinct neighbourhood
+-- FROM final_airbnb
 
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
-
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>> DONE
 -- Find both the highest price listing and the lowest price listing, displaying the entire row for each
 -- HINT: This can be two different queries.
-
 -- FOOD FOR THOUGHT: Think about the results. Are the high and low prices outliers in this data set?
-
 -- EXPECTED OUTPUT: Highest = 785, Lowest = 55
 
+-- ---------- SOLUTIONS ----------
+-- SELECT * FROM final_airbnb
+-- WHERE price = (SELECT MAX(price) FROM final_airbnb);
 
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
+-- SELECT * FROM final_airbnb
+-- WHERE price = (SELECT MIN(price) FROM final_airbnb);
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>> DONE
 -- Find the average availability for all listings in the data set (using the availability_365 column)
 -- HINT: Aggregates are more than just big rocks...
-
 -- EXPECTED OUTPUT: 165.3904
 
+-- ---------- SOLUTION ----------
+-- SELECT AVG(availability_365) FROM final_airbnb
 
--- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>> 
 -- Find all listings that do NOT have a review
 -- HINT: There are a few ways to go about this. Remember that an empty cell is "no value", but not necessarily NULL
-
 -- EXPECTED OUTPUT: 6 rows
-
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 7 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
 -- HINT: Sorting is your friend!
-
 -- EXPECTED OUTPUT: 58059
-
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 8 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the most popular neighbourhood for listings 
 -- HINT: Look for which neighbourhood appears most frequently in the neighbourhood column
 -- HINT: You are creating "summary rows" for each neighbourhood, so you will just see one entry for each neighbourhood
-
 -- EXPECTED OUTPUT: Williamsburg
 -- INVESTIGATE: Should Williamsburg be crowned the most popular neighbourhood?
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 9 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
-
 -- EXPECTED OUTPUT: 58059
-
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 10 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out which host has the most listings. 
 -- Create a NEW column that will show a calculation for how many listings the host for each listing has in the table
 -- Display the column using aliasing.
 -- HINT: Work this one step at a time. See if you can find a way to just display the count of listings per host first.
-
 -- EXPECTED OUTPUT: The Box House Hotel with 6 listings
-
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 11 >>>>>>>>>>>>>>>>>>>>>>>
 -- <<<<<<<<<<<<<<<<<<<<<<< WRAP UP >>>>>>>>>>>>>>>>>>>>>>>>>
